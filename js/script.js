98651ce7566a86decb6107eea301e6cf3da4959b
emetdas <emet@b2bleadstaff.com>
2021-09-27 18:40:54 +0600

    update is done

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// navigation-start
+let menu_icon = document.querySelector('.menu_icon');
+let menu_icon_box = document.querySelector('.menu_icon_box');
+let nav_menu = document.querySelector('.nav_menu');
+let nav_link = document.querySelectorAll('.nav_link');
+
+menu_icon.addEventListener('click', () => {
+  menu_icon_box.classList.toggle('active');
+  nav_menu.classList.toggle('active');
+});
+nav_link.forEach((e) => {
+  e.addEventListener('click', () => {
+    menu_icon_box.classList.remove('active');
+    nav_menu.classList.remove('active');
+  });
+});
+
+// navigation-end
